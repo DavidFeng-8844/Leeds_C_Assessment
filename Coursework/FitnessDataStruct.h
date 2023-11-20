f6b15e1bbcfcda5d9b8e382741b0d7a9215c5675
davids-Ubuntu22 <davidfeng.8844@gmail.com>
2023-11-20 14:54:56 +0800

    edit the FitnessDataStruct.h

--- Coursework/FitnessDataStruct.h
+++ Coursework/FitnessDataStruct.h
@@ -8,5 +8,6 @@ typedef struct {
 } FITNESS_DATA;
 
 // Declare my helper functions here
+void tokeniseRecord(const char *input, const char *delimiter, char *date, char *time, char *steps);
 
 #endif
